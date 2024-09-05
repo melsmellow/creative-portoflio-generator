36c99986666306be570f01ab33a4654059935479
Mel Carlo <melcarlo.iguis@gmail.com>
2024-09-05 22:09:02 +0800

    hard coded value

--- server-action/bucket.tsx
+++ server-action/bucket.tsx
@@ -4,15 +4,16 @@ import axios from "axios";
 
 export async function uploadFile(file: File) {
   const s3 = new S3Client({
-    endpoint: process.env.BUCKET_ENDPOINT,
-    region: "us-east-005",
+    endpoint: "https://s3.us-east-005.backblazeb2.com/", // Your Backblaze B2 S3-compatible endpoint
+    region: "us-east-005", // Your Backblaze B2 region
     credentials: {
-      accessKeyId: process.env.B2_APPLICATION_KEY!,
-      secretAccessKey: process.env.BUCKET_KEY_ID!,
+      accessKeyId: "00503f44c8bf2aa0000000002",
+      secretAccessKey: "K005IZx9MlwLqAT4R6UIuu/tzPrP6ko",
     },
   });
   console.log(s3);
 
+  // Step 2: Upload the file
   try {
     const expiresIn = 7 * 24 * 60 * 60; // 3600
     const command = new PutObjectCommand({
@@ -21,6 +22,17 @@ export async function uploadFile(file: File) {
     });
     const signedUrl = await getSignedUrl(s3, command, { expiresIn });
     const result = await axios.put(signedUrl, file);
+
+    console.log(result);
+    // const uploadResult = await s3.send(
+    //   new PutObjectCommand({
+    //     Bucket: "melsmellow", // Your Backblaze B2 bucket name
+    //     Key: file.name, // The key (filename) under which the file will be stored in the bucket
+    //     Body: file, // The file data
+    //     ContentType: file.type, // The MIME type of the file
+    //   })
+    // );
+    // console.log("Upload successful:", uploadResult);
   } catch (error) {
     console.error("Error uploading image:", error);
   }
